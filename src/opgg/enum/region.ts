73cf6ecf10c30535dfc3b6cb7de5c494c9293b97
anderson202 <andersecng@gmail.com>
2020-10-24 14:49:59 -0400

    Move scraper from frontend package

---
+++ src/opgg/enum/region.ts
@@ -0,0 +1,13 @@
+export enum Region {
+    NA = 'na',
+    EUNE = 'eune',
+    LAN = 'lan',
+    KR = 'kr',
+    OCE = 'oce',
+    RU = 'ru',
+    JP = 'jp',
+    BR = 'br',
+    TR = 'tr',
+    EUW = 'euw',
+    LAS = 'las',
+}
